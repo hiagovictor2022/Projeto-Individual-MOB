f585a740ef29ac548fc66e50f067561cb9db4372
hiagovictor2022 <hiago.rodrigues@sptech.school>
2022-06-07 22:43:47 -0300

    novo banco

--- banco/script_mob.sql
+++ banco/script_mob.sql
@@ -2,7 +2,7 @@ CREATE DATABASE projetoMob;
 
 USE projetoMob;
 
-CREATE TABLE usuario (
+CREATE TABLE usuario(
 	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
@@ -10,16 +10,59 @@ CREATE TABLE usuario (
 );
 
 create table voto_personagens(
+id_voto int auto_increment,
 mob int,
 ritsu int,
 teruki int,
 reigen int,
 covinhas int,
-musashi int
+musashi int,
+fk_usuario int,
+foreign key (fk_usuario) references usuario (id),
+primary key (id_voto,fk_usuario)
 );
+
+drop table votos;
+
+INSERT INTO voto_personagens (mob,fk_usuario) VALUES (1,1);
 
 select * from voto_personagens;
 
+select truncate((sum(mob)/count(id_voto))*100,2) as porcentagem from voto_personagens;
 
- -- fk_usuario int,foreign key (fk_usuario) references usuario (id)
+
+create table votos (
+fk_usuario int,
+foreign key (fk_usuario) references usuario (id),
+personagem varchar(15),
+primary key(fk_usuario,personagem)
+);
+
+insert into usuario values 
+
+(null,'hiago','hiago@gmail.com','1020');
+
+insert into votos values 
+(1,'mob'),
+(1,'covinhas');
+
+insert into usuario values 
+
+(null,'joao','joao@gmail.com','1020');
+insert into votos values 
+(2,'mob'),
+(2,'covinhas'),
+(2,'ritsu');
+
+select * from votos;
+
+select count(personagem) as qtd_votos,(select count(fk_usuario) from votos) as qtd_total from votos where personagem = 'mob';
+
+select personagem,count(personagem) from votos group by personagem;
+
+
+
+
+
+
 
